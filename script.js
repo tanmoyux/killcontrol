6f85dcbf75135633473be89d4dcc7a26c3a62936
tanmoyux <78644667+tanmoyux@users.noreply.github.com>
2024-05-20 20:51:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ accordions.forEach((acco) => {
     }
   };
 });
+
+/* Cookie Banner */
+$(document).ready(function() {
+  cookieBanner.init();
+  $('#delete-cookies-button').click(function() {
+      deleteAllCookies();
+      alert('Alle Cookies wurden gelöscht');
+      location.reload(); // Seite neu laden
+  });
+});
+
+function deleteAllCookies() {
+  var cookies = document.cookie.split(";");
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+  }
+}
